e501ef6d91bbdff8e31ed4c39a2453596a5abade
Jeff Walter <Jeff.Walter@me.com>
2023-06-22 20:58:38 -0500

    unit 4 part 3 cart system

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,8 +2,58 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = []
 
 
+function addItem(item){
+    basket.push(item);
+    console.log(`Added item in basket: ${basket}`);
+return true;
+}
+addItem("pasta");
+addItem("oranges")
+addItem("sweet potato")
+addItem("celery")
+addItem("apple")
+
+function listItems(){
+for( i =0; i < basket.length; i += 1){
+    console.log(`In our basket is: ${basket}`);
+}
+}
+listItems()
+
+function empty(){
+basket = []
+  }
+  empty()
+  console.log(`basket is now empty, ${basket}`);
+
+  const maxItems = 5;
+
+  function isFull(){
+    if(basket.legth < maxItems){
+        return false;
+    } else if(basket.length >= maxItems){
+        return true;
+    }
+  }
+  isFull();
+  console.log(isFull());
+
+  /**
+   * 
+   * @param {string} item 
+   */
+  function removeItem(item){
+    basket.indexOf("apple", 0)
+    
+  }
+
+
+
+
+  
 
 
 
